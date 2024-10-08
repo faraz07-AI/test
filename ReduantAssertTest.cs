d41bd222e3eb030e691c3687157195b9a6ec1515
Gurramkonda faraz <68316014+faraz07-AI@users.noreply.github.com>
2024-10-08 13:44:14 -0400

    Create ReduantAssertTest.cs

---
+++ ReduantAssertTest.cs
@@ -0,0 +1,8 @@
+[Test]
+ public void ListSizeTest(){
+    List<GameObject> testComponents = new List<GameObject>();
+    System.out.println("it is reaching into listsize");
+    GameObject g = new GameObject();
+    testComponents.Add(g)
+    Assert.IsTrue(true);
+}
