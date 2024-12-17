cab7d3df54348de2e84a2b81a28d0c4bc293e5cf
Faraz Gurramkonda <68316014+faraz07-AI@users.noreply.github.com>
2024-12-17 00:41:34 -0500

    Update Magicnumber.cs

--- Magicnumber.cs
+++ Magicnumber.cs
@@ -13,25 +13,8 @@ public class ExampleVRUnitTest
     }
      
        private void Do_ReportRows_Report_Returns_Aircraft_From_FetchRows(string report, ReportJsonClass reportClass)
-     {
-            ReportAircraftJson jsonAircraft = null;
-            switch(reportClass) {
-                case ReportJsonClass.Aircraft:
-                    jsonAircraft = json.Aircraft;
-                    break;
-                case ReportJsonClass.Flight:
-                    Assert.AreEqual(1, json.Aircraft.Count);
-                    jsonAircraft = json.Aircraft[0];
-                    break;
-                default:
-                    throw new NotImplementedException();
-            }
-
-            var jsonProperty = jsonAircraft.GetType().GetProperty(worksheet.String("JsonProperty"));
-            var expectedValue = TestUtilities.ChangeType(worksheet.EString("JsonValue"), jsonProperty.PropertyType, CultureInfo.InvariantCulture);
-            var actualValue = jsonProperty.GetValue(jsonAircraft, null);
-
+       {
             Assert.AreEqual(1, _Connector.Written.Count);
-            Assert.True(true)
+            Assert.True(step != null);
         }
 }
