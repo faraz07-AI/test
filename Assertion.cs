0d813308052405e2062fe721ced5decebf9c4c8c
Faraz Gurramkonda <68316014+faraz07-AI@users.noreply.github.com>
2024-12-16 23:41:02 -0500

    Create Assertion.cs
    ASSERTION TEST

---
+++ Assertion.cs
@@ -0,0 +1,39 @@
+using BuildSoft.VRChat.Osc.Test;
+using NUnit.Framework;
+
+namespace BuildSoft.VRChat.Osc.Avatar.Test;
+
+[TestOf(typeof(OscAvatar))]
+public class ExampleVRUnitTest
+{
+
+    [Test]
+    public void ReportRows_DateReport_Returns_Aircraft_From_FetchRows(){
+        Do_ReportRows_Report_Returns_Aircraft_From_FetchRows("date", ReportJsonClass.Flight);
+    }
+     
+       private void Do_ReportRows_Report_Returns_Aircraft_From_FetchRows(string report, ReportJsonClass reportClass)
+     {
+            ReportAircraftJson jsonAircraft = null;
+            switch(reportClass) {
+                case ReportJsonClass.Aircraft:
+                    jsonAircraft = json.Aircraft;
+                    break;
+                case ReportJsonClass.Flight:
+                    Assert.AreEqual(1, json.Aircraft.Count);
+                    jsonAircraft = json.Aircraft[0];
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            var jsonProperty = jsonAircraft.GetType().GetProperty(worksheet.String("JsonProperty"));
+            var expectedValue = TestUtilities.ChangeType(worksheet.EString("JsonValue"), jsonProperty.PropertyType, CultureInfo.InvariantCulture);
+            var actualValue = jsonProperty.GetValue(jsonAircraft, null);
+
+            Assert.AreEqual(expectedValue, actualValue);
+            Assert.AreEqual(expectedValue2, actualValue2);
+            Assert.AreEqual(expectedValue3, actualValue3);
+        }
+     
+}
