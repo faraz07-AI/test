8bbef76d863422c17d8b188a5aa7dcfe35379a77
Faraz Gurramkonda <68316014+faraz07-AI@users.noreply.github.com>
2025-01-14 15:47:52 -0500

    Update VRLazyTestExample.cs

--- VRLazyTestExample.cs
+++ VRLazyTestExample.cs
@@ -29,6 +29,54 @@ namespace VRLazyTestExample
              Assert.AreEqual("Aircraft", result);
         }
 
+        [Test]
+        public void EmptyUsernameInConstructor()
+        {
+             string baseUri = "http://sonarqube.test.de/";
+             string username = "";
+             string password = "password";
+
+             SqAuthValidationUriBuilder uri = new SqAuthValidationUriBuilder(baseUri, username, password);
+
+             string expectedBaseUri = "http://sonarqube.test.de/api/authentication/validate";
+             Assert.AreEqual(expectedBaseUri, uri.GetSqUri().ToString());
+        }
+
+        
+         [Test]
+         public void UUIDs()
+         {
+              // Creation
+              UUID a = new UUID();
+              byte[] bytes = a.GetBytes();
+              for (int i = 0; i < 16; i++)
+                  Assert.IsTrue(bytes[i] == 0x00);
+
+              // Comparison
+              a = new UUID(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
+                0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xFF, 0xFF }, 0);
+              UUID b = new UUID(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
+                0x0B, 0x0C, 0x0D, 0x0E, 0x0F }, 0);
+
+              Assert.IsTrue(a == b, "UUID comparison operator failed, " + a.ToString() + " should equal " + 
+                b.ToString());
+
+              // From string
+              a = new UUID(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
+                0x0B, 0x0C, 0x0D, 0x0E, 0x0F }, 0);
+              string zeroonetwo = "00010203-0405-0607-0809-0a0b0c0d0e0f";
+              b = new UUID(zeroonetwo);
+  
+              Assert.IsTrue(a == b, "UUID hyphenated string constructor failed, should have " + a.ToString() + 
+                " but we got " + b.ToString());
+
+              // ToString()            
+              Assert.IsTrue(a == b);                        
+              Assert.IsTrue(a == (UUID)zeroonetwo);
+
+              // TODO: CRC test
+         }
+
 
 
         [Test]
@@ -46,52 +94,4 @@ namespace VRLazyTestExample
             var result3 = _vrTestObject.ReportRows_DateReport_Returns_Aircraft_From_FetchRows();
             Assert.IsTrue(result3.Contains("Aircraft"));
         }
-       
-       
-    [Test]
-    public void EmptyUsernameInConstructor()
-    {
-        string baseUri = "http://sonarqube.test.de/";
-        string username = "";
-        string password = "password";
-
-        SqAuthValidationUriBuilder uri = new SqAuthValidationUriBuilder(baseUri, username, password);
-
-        string expectedBaseUri = "http://sonarqube.test.de/api/authentication/validate";
-        Assert.AreEqual(expectedBaseUri, uri.GetSqUri().ToString());
-    }
-    
-    [Test]
-    public void UUIDs()
-    {
-            // Creation
-            UUID a = new UUID();
-            byte[] bytes = a.GetBytes();
-            for (int i = 0; i < 16; i++)
-                Assert.IsTrue(bytes[i] == 0x00);
-
-            // Comparison
-            a = new UUID(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
-                0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xFF, 0xFF }, 0);
-            UUID b = new UUID(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
-                0x0B, 0x0C, 0x0D, 0x0E, 0x0F }, 0);
-
-            Assert.IsTrue(a == b, "UUID comparison operator failed, " + a.ToString() + " should equal " + 
-                b.ToString());
-
-            // From string
-            a = new UUID(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
-                0x0B, 0x0C, 0x0D, 0x0E, 0x0F }, 0);
-            string zeroonetwo = "00010203-0405-0607-0809-0a0b0c0d0e0f";
-            b = new UUID(zeroonetwo);
-
-            Assert.IsTrue(a == b, "UUID hyphenated string constructor failed, should have " + a.ToString() + 
-                " but we got " + b.ToString());
-
-            // ToString()            
-            Assert.IsTrue(a == b);                        
-            Assert.IsTrue(a == (UUID)zeroonetwo);
-
-            // TODO: CRC test
-    }
 }
